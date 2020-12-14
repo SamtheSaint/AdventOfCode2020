9bdfb7a34f2a34acecc91ebcf8ce191ec62cb1f2
Samuel Adekunle <me@samuel-adekunle.software>
2020-12-14 08:16:32 +0000

    feat: completed second part day 14
    Signed-off-by: Samuel Adekunle <me@samuel-adekunle.software>

--- 14/main.cpp
+++ 14/main.cpp
@@ -4,30 +4,50 @@
 #include <regex>
 #include <string>
 #include <cstdint>
+#include <stack>
+
+#define floating 'X'
 
 using namespace std;
 
 int main(int argc, char const* argv[])
 {
   unordered_map<uint64_t, uint64_t> mem;
-  uint64_t ands = 0xFFFFFFFFF; // initally all ones
-  uint64_t ors = 0x0; // initially all zeros
+  string mask;
 
-  auto updateMask = [&ands, &ors](const string& mask) {
-    ands = 0xFFFFFFFFF; // reset mask
-    ors = 0x0; // reset mask
-
+  auto applyMask = [&mask](uint64_t address) {
+    string addressMask = mask;
     for (int i = 0; i < mask.size(); i++) {
-      if (mask[i] == 'X') continue;
-      else if (mask[i] == '1') ors |= (uint64_t)1 << (35 - i);
-      else if (mask[i] == '0') ands &= ~((uint64_t)1 << (35 - i));
+      if (mask[i] == '0') {
+        bool bit = (address >> (mask.size() - i - 1)) & 1;
+        addressMask[i] = bit ? '1' : '0';
+      }
     }
+    return addressMask;
   };
 
-  auto writeToMemory = [&mem, &ands, &ors](const uint64_t address, uint64_t data) {
-    data &= ands; // set zeros
-    data |= ors; // set ones
-    mem[address] = data;
+  auto writeToMemory = [&mem, &applyMask](const uint64_t address, uint64_t data) {
+    string addressMask = applyMask(address);
+    stack<string> tmp;
+    tmp.push(addressMask);
+    while (!tmp.empty()) {
+      string cur = tmp.top();
+      tmp.pop();
+
+      size_t index = cur.find(floating);
+      if (index == string::npos) {
+        uint64_t addr = stoull(cur, 0, 2);
+        mem[addr] = data;
+      }
+      else {
+        string cur0 = cur;
+        string cur1 = cur;
+        cur0[index] = '0';
+        cur1[index] = '1';
+        tmp.push(cur0);
+        tmp.push(cur1);
+      }
+    }
   };
 
   auto maskPattern = regex("mask = ([X10]{36})");
@@ -37,7 +57,7 @@ int main(int argc, char const* argv[])
   while (getline(cin, s)) {
     if (s.empty()) continue;
     if (regex_match(s, m, maskPattern)) {
-      updateMask(m[1].str());
+      mask = m[1].str();
       continue;
     }
     if (regex_match(s, m, dataPattern)) {
@@ -47,10 +67,10 @@ int main(int argc, char const* argv[])
       continue;
     }
   }
-  long long acc = 0;
+  unsigned long long acc = 0;
   for (const auto& kv : mem) {
     acc += kv.second;
-    cout << kv.first << ": " << kv.second << endl;
+    // cout << kv.first << ": " << kv.second << endl;
   }
   cout << acc << endl;
   return 0;
