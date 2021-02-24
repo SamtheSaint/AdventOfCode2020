02d65992591a3654dc1b19aa338def4e1c9162b1
Samuel Adekunle <me@samuel-adekunle.software>
2021-02-24 09:04:11 +0000

    uploading to github

---
+++ 19/main.cpp
@@ -0,0 +1,30 @@
+// Copyright 2020 Samuel-Adekunle.Software
+// Project: AdventOfCode2020
+// Author: Samuel Adekunle
+// Created: 19/12/2020, 09:48:41
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stack>
+#include <cstdint>
+#include <cassert>
+#include <regex>
+
+struct Rule {
+  size_t length;
+  std::regex re;
+};
+
+
+int main(int argc, char const* argv[])
+{
+  std::vector<Rule> rules;
+  std::string s;
+
+  while (getline(std::cin, s)) {
+    
+  }
+  return 0;
+}
